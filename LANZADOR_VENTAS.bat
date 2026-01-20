c285e99b5ec26727c913d526982f2cb0281240cd
Rodrigo <ojedaccoyorodrigo@gmail.com>
2026-01-20 17:22:12 -0500

    Cleanup: Removed local launchers and temp files

--- LANZADOR_VENTAS.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Lanzador de Aplicacion de Ventas - Viajes Cusco Peru
-echo Iniciando servidor de itinerarios...
-echo Por favor, no cierres esta ventana mientras uses la aplicacion.
-python -m streamlit run c:\Itinerario\App_Ventas.py
-pause
